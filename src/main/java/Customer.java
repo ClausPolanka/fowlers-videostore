6c9614a517d0e30913d636395ee300d4fd30c45b
Claus Polanka <claus.polanka@chello.at>
2011-04-07 18:14:24 +0200

    Initial project structure for thecleancoders episode

---
+++ src/main/java/Customer.java
@@ -0,0 +1,78 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+//From book: 'Refactoring' by Martin Fowler
+//This is the original code before refactoring begins
+
+public class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+    private double totalAmount;
+
+    private int frequentRenterPoints;
+
+    public Customer(String name) {
+		_name = name;
+	}
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String statement() {
+        totalAmount = 0;
+        frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+
+		while (rentals.hasMoreElements()) {
+			double thisAmount = 0;
+			Rental each = (Rental) rentals.nextElement();
+
+			//determine amounts for each line
+			switch (each.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2)
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3)
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+			}
+
+			// add frequent renter points
+			frequentRenterPoints++;
+			// add bonus for a two day new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+				frequentRenterPoints++;
+
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+
+		// add footer lines
+		result += "You owed " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+
+		return result;
+	}
+
+    public double getTotal() {
+        return totalAmount;
+    }
+
+    public int getFrequentRenterPoints() {
+        return frequentRenterPoints;
+    }
+}
